33d63bdbde664465dc6c633283671003f260666a
Alejandro Velasquez <velasale@oregonstate.edu>
2022-03-16 23:56:53 -0700

    setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,23 @@
-# @Time : 3/16/2022 7:25 PM
-# @Author : Alejandro Velasquez
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pickApp",
+    version="0.0.1",
+    author="Alejandro Velasquez",
+    author_email="velasale@oreognstate.edu",
+    description="Real and Proxy apple picks experiments",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/velasale/PickApp",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
