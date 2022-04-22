48c3dcb55cc54e577b1504be8d0160bb60a2dd6b
Alejandro Velasquez <velasale@oregonstate.edu>
2022-04-21 17:55:49 -0700

    Putting together all the post-processing scripts into a single file

--- temporal/appickdata.py
+++ temporal/appickdata.py
@@ -115,29 +115,75 @@ def down_sample(period, source, target):
 
 
 def join_csv(name, case, source, target):
+    """
+    Joins csv from different topics but from the same experiment, into a single csv.
+    Thus, data is easier to handle, and less prone to make mistakes.
+    It does some cropping of the initial or last points, in order to have all the topics have the same size
+    :param name: Name of the experiment
+    :param case: Whether Grasp or Pick stage
+    :param source:
+    :param target:
+    :return:
+    """
 
     if case == 'GRASP/':
         stage = 'grasp'
     elif case == 'PICK/':
         stage = 'pick'
 
+    # --- Step 1: Open all the topics from the same experiment that need to be joined ---
     location = source
-
-    # Open all the topics to join
     topics = ['_wrench', '_f1_imu', '_f1_states', '_f2_imu', '_f2_states', '_f3_imu', '_f3_states']
 
-    data_0 = pd.read_csv(location + name + stage + topics[0] + '.csv', header=None, index_col=False).iloc[:, 1:]
-    data_1 = pd.read_csv(location + name + stage + topics[1] + '.csv', header=None, index_col=False).iloc[:, 1:]
-    data_2 = pd.read_csv(location + name + stage + topics[2] + '.csv', header=None, index_col=False).iloc[:, 1:]
-    data_3 = pd.read_csv(location + name + stage + topics[3] + '.csv', header=None, index_col=False).iloc[:, 1:]
-    data_4 = pd.read_csv(location + name + stage + topics[4] + '.csv', header=None, index_col=False).iloc[:, 1:]
-    data_5 = pd.read_csv(location + name + stage + topics[5] + '.csv', header=None, index_col=False).iloc[:, 1:]
-    data_6 = pd.read_csv(location + name + stage + topics[6] + '.csv', header=None, index_col=False).iloc[:, 1:]
+    data_0 = pd.read_csv(location + name + stage + topics[0] + '.csv', header=None, index_col=False)
+    data_1 = pd.read_csv(location + name + stage + topics[1] + '.csv', header=None, index_col=False)
+    data_2 = pd.read_csv(location + name + stage + topics[2] + '.csv', header=None, index_col=False)
+    data_3 = pd.read_csv(location + name + stage + topics[3] + '.csv', header=None, index_col=False)
+    data_4 = pd.read_csv(location + name + stage + topics[4] + '.csv', header=None, index_col=False)
+    data_5 = pd.read_csv(location + name + stage + topics[5] + '.csv', header=None, index_col=False)
+    data_6 = pd.read_csv(location + name + stage + topics[6] + '.csv', header=None, index_col=False)
 
-    # Join them all into a single DataFrame
-    df = pd.concat([data_0, data_1, data_2, data_3, data_4, data_5, data_6], axis=1)
+    dataframes = [data_0, data_1, data_2, data_3, data_4, data_5, data_6]
 
-    # Target Location
+    # --- Step 2: Crop initial or last points in order to make all topics have the same length
+    # Get the channel with the less sampled points
+    smallest = 10000
+    for channel in dataframes:
+        if channel.shape[0] < smallest:
+            smallest = channel.shape[0]
+            benchmark = channel
+    # print("\nSmallest:", smallest)
+
+    benchmark_first = float(benchmark.iloc[1, 0])  # First Reading
+    benchmark_last = float(benchmark.iloc[-1, 0])  # Last Reading
+    # print("First and last", benchmark_first, benchmark_last)
+
+    count = 0
+    for channel in dataframes:
+        if channel.shape[0] > smallest:
+            difference = channel.shape[0] - smallest
+            print("The difference is", difference)
+
+            # Check which points to crop, the initial or last ones
+            initial_time_offset = abs(float(channel.iloc[1, 0]) - benchmark_first)
+            last_time_offset = abs(float(channel.iloc[-1, 0]) - benchmark_last)
+
+            if initial_time_offset > last_time_offset:
+                print("Remove initial")
+                for i in range(difference):
+                    new_df = channel.drop([1]).reset_index(drop=True)
+            else:
+                print("Remove last")
+                new_df = channel.iloc[:-difference, :]
+
+            dataframes[count] = new_df
+
+        count = count + 1
+
+    # --- Step 3: Join them all into a single DataFrame and save
+    df = pd.concat([dataframes[0].iloc[:, 1:], dataframes[1].iloc[:, 1:], dataframes[2].iloc[:, 1:],
+                    dataframes[3].iloc[:, 1:], dataframes[4].iloc[:, 1:], dataframes[5].iloc[:, 1:],
+                    dataframes[6].iloc[:, 1:]], axis=1)
     new_file_name = target + name + '_' + str(stage) + '.csv'
     df.to_csv(new_file_name, index=False, header=False)
 
@@ -164,8 +210,8 @@ def main():
     # (pp) real_pick_delCol.py
 
     main = 'C:/Users/15416/Box/Learning to pick fruit/Apple Pick Data/RAL22 Paper/'
-    # dataset = '3_proxy_winter22_x1/'
-    dataset = '5_real_fall21_x1/'
+    dataset = '3_proxy_winter22_x1/'
+    # dataset = '5_real_fall21_x1/'
     # dataset = '1_proxy_rob537_x1/'
     stages = ['GRASP/', 'PICK/']
 
@@ -174,16 +220,15 @@ def main():
         location = main + dataset + stage
         location_1 = location + 'pp1_split/'
         location_2 = location + 'new_pp2_downsampled/'
-        location_3 = location + 'new_pp3_cropped/'
-        location_4 = location + 'new_pp4_joined/'
-        location_5 = location + 'new_pp5_labeled/'
+        location_3 = location + 'new_pp3_joined/'
+        location_4 = location + 'new_pp4_labeled/'
 
         # --- Step 4: Down sample Data ---
         period = 15  # Sampling period in [ms]
         # down_sample(period, location_1, location_2)
 
         # --- Step 5: Crop Data ---
-        l, h = check_size(location_2)
+        # l, h = check_size(location_2)
         # print(dataset, stage, l, h)
         if stage == 'GRASP/':
             size = 106
@@ -197,24 +242,23 @@ def main():
     # (pp) csv_joiner.py
     metadata_loc = main + dataset + 'metadata/'
 
-    # for filename in tqdm(sorted(os.listdir(metadata_loc))):
-    #
-    #     # Get the basic name
-    #     name = str(filename)
-    #     start = name.index('app')
-    #     end = name.index('m')
-    #     name = name[start:end]
-    #     # print(name)
-    #
-    #     for stage in stages:
-    #         location = main + dataset + stage
-    #         location_3 = location + 'new_pp3_cropped/'
-    #         location_4 = location + 'new_pp4_joined/'
-    #         join_csv(name, stage, location_3, location_4)
+    for filename in tqdm(sorted(os.listdir(metadata_loc))):
 
+        # Get the basic name
+        name = str(filename)
+        start = name.index('app')
+        end = name.index('m')
+        name = name[start:end]
+        print(name)
+
+        for stage in stages:
+            print(stage)
+            location = main + dataset + stage
+            location_2 = location + 'new_pp2_downsampled/'
+            location_3 = location + 'new_pp3_joined/'
+            join_csv(name, stage, location_2, location_3)
 
     # --- Step 7: Augment Data ---
-
 
     # Step 6 - Do Data Augmentation by adding Noise
     # TODO
